b784c5e55008f51e4f2b9830253fbf6ca137b880
HP <HP@DESKTOP-6MCP70J>
2021-05-05 12:58:32 +0530

    first commit message

---
+++ SeleniumAutomation/src/test/java/com/qa/amazonsearch/Amazon.java
@@ -0,0 +1,52 @@
+package com.qa.amazonsearch;
+	import java.io.IOException;
+	import java.util.Set;
+
+	import org.openqa.selenium.Keys;
+	import org.testng.annotations.DataProvider;
+	import org.testng.annotations.Test;
+
+	import com.qa.utility.ExcelUtility;
+
+	public class Amazon extends TestBase
+	{
+		 @Test(dataProvider="getData")
+		  public void amazonShop(String input) throws InterruptedException
+		  {
+			 Thread.sleep(30000);
+			  page.getTypetosearch().sendKeys(input);
+			  page.getTypetosearch().sendKeys(Keys.ENTER);
+			 
+		      page.getSelecting().click();
+		      
+		      Set<String> window=driver.getWindowHandles();
+		      String handle=null;
+		      for(String shift : window)
+		      {
+		    	  handle=shift;
+		      }
+		      driver.switchTo().window(handle);
+		      
+			  page.getCartAdd().click();
+			  page.getHome().click();
+			  
+		  }
+			@DataProvider
+			public Object[][] getData() throws IOException
+			{
+				String xlPath="C:\\Users\\HP\\eclipse-workspace\\SeleniumAutomation\\src\\test\\java\\com\\qa\\testdata1\\TestData.xlsx";
+				String xlSheet="Sheet1";
+			   int rowCount=ExcelUtility.getRowCount(xlPath, xlSheet);
+			   int cellCount=ExcelUtility.getCellCount(xlPath, xlSheet, rowCount);
+			   String[][] data= new String[rowCount][cellCount];
+			   for(int i=1;i<=rowCount;i++)
+			   {
+				   for(int j=0;j<cellCount;j++)
+				   {
+					   data[i-1][j]=ExcelUtility.getcellData(xlPath, xlSheet, i, j);
+				   }
+			   }
+			   return data;
+			}
+
+}
